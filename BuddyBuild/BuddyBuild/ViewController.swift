8646f4bc52cb606bf68804564f11609c82353199
Andrey Popov <apopov@shakuro.com>
2017-10-24 09:52:02 +0300

    initial

---
+++ BuddyBuild/BuddyBuild/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuddyBuild
+//
+//  Created by Andrey on 24/10/2017.
+//  Copyright © 2017 Shakuro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
